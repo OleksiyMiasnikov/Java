fd47f1dc5d3208511ca80800b25a56d567bcb89a
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2022-05-26 23:09:34 +0300

    RandomNumbers added #1

---
+++ RandomNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumbers {
+    //Вывести заданное количество случайных чисел с переходом и без перехода на новую строку
+
+    public static void main(String[] args) {
+        int qN;
+
+        do {
+            System.out.print("Input quantity of numbers, please (1 - 10):");
+            Scanner scanner = new Scanner(System.in);
+            qN = scanner.nextInt();
+        } while (qN < 1 || qN > 10);
+
+        Random random = new Random();
+        for (int i = 1; i <= qN; i++) {
+            System.out.println(random.nextInt());
+        }
+
+    }
+}

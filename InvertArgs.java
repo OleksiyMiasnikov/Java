7c83630b2f72a781ed8ff378857feec312945568
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2022-05-26 23:06:09 +0300

    InvertArgs added #1

---
+++ InvertArgs.java
@@ -0,0 +1,22 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Stack;
+
+public class InvertArgs {
+    //Отобразить в окне консоли аргументы командной строки в обратном порядке.
+
+    public static void main(String @NotNull [] args) {
+        if (args.length == 0) {
+            System.out.println("There is no arguments!!!");
+            return;
+        }
+        Stack<String> stackArgs = new Stack<>();
+        for (String elem: args) {
+            stackArgs.push(elem);
+        }
+        while (!stackArgs.empty()){
+            System.out.println(stackArgs.pop());
+        }
+
+    }
+}

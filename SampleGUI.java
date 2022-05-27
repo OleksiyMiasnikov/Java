cc3879f466cbe9d78587415ab06f8167c5c1dd01
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2022-05-27 10:46:01 +0300

    added SampleGUI #1

---
+++ SampleGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class SampleGUI {
+
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Input your name :");
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Input your age :"));
+        double hight = Double.parseDouble(JOptionPane.showInputDialog("Input your hight :"));
+        JOptionPane.showMessageDialog(null,"Hello " + name + "!\n You are " + age + " yars old.\n Your hight is " + hight + " cm");
+    }
+}

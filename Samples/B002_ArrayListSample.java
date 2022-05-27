e058f63b9c21fd6d987ee4034d069520b9b12706
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2022-05-27 22:21:51 +0300

    added ArrayListSample #2

--- Samples/B002_ArrayListSample.java
+++ Samples/B002_ArrayListSample.java
@@ -7,26 +7,29 @@ public class B002_ArrayListSample {
     public static void main(String[] args) {
 
         ArrayList<String> food = new ArrayList<>();
-
         food.add("apple");
         food.add("bread");
         food.add("cheese");
         food.add("burger");
         food.add("pizza");
 
-
-        for (String element : food) {
-            System.out.println(element);
-        }
+        System.out.println(food);
 
         food.remove(4);
         food.set(2,"sausages");
 
-        System.out.println();
+        System.out.println(food);
 
-        for (String element : food){
-            System.out.println(element);
-        }
+        ArrayList<String> drinks = new ArrayList<>();
+        drinks.add("juice");
+        drinks.add("coffee");
+        drinks.add("beer");
+
+        ArrayList<ArrayList<String>> shoppingList = new ArrayList<>();
+        shoppingList.add(food);
+        shoppingList.add(drinks);
+
+        System.out.println("Shopping list :" + shoppingList);
     }
 
  }

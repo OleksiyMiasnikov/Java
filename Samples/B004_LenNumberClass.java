389566340d41475d489ff4fb6d3b56620bb7c3bc
Oleksiy Miasnikov <oleksiymiasnikov@gmail.com>
2022-05-28 11:12:13 +0300

    added file LenNumberClass #1

---
+++ Samples/B004_LenNumberClass.java
@@ -0,0 +1,63 @@
+package Samples;
+
+import java.util.Scanner;
+
+class LenNumber {
+    int number;
+
+    public LenNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int len(){
+        int number = this.number;
+        int res = 0;
+        while (number != 0) {
+            number /= 10;
+            res += 1;
+        }
+        return res;
+    }
+
+    public int len(int number){
+        int res = 0;
+        while (number != 0) {
+            number /= 10;
+            res += 1;
+        }
+        return res;
+    }
+    public int compare (int comparable) {
+        int ln = len();
+        int lc = len(comparable);
+        return (ln < lc) ? -1 : ((ln == lc) ? 0 : 1);
+    }
+}
+
+public class B004_LenNumberClass {
+
+    public static void main(String[] args) {
+        int number = 15567;
+        LenNumber lNumber = new LenNumber(number);
+        System.out.printf("Length of my number (%d) is : %d.\n", number, lNumber.len());
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your number, please :");
+        int newNumber = scanner.nextInt();
+        int res = lNumber.compare(newNumber);
+        if (res == 1){
+            System.out.println("My number is longer than yours!");
+        } else if (res == -1){
+            System.out.println("Your number is longer than mine!");
+        } else {
+            System.out.println("Our numbers are the same length!");
+        }
+    }
+}
